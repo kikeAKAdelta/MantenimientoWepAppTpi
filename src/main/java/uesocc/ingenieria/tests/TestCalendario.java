eabf2891d8c1f8c6695dbcf7119b9720fd18162c
ricky <ricky@debian>
2018-03-29 13:35:20 -0600

    Creando el entityManager de Calendario y probandolo

--- src/main/java/uesocc/ingenieria/tests/TestCalendario.java
+++ src/main/java/uesocc/ingenieria/tests/TestCalendario.java
@@ -4,21 +4,24 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceUnit;
 import uesocc.ingenieria.entity.Calendario;
 
 
 public class TestCalendario {
     
     private static EntityManager manager;
+    
     private static EntityManagerFactory emf;
     
     public static void main(String[] args){
         //Se crea el gestor de persistencia
-        Persistence.createEntityManagerFactory("Mantenimiento");
+        emf = Persistence.createEntityManagerFactory("Mantenimiento");
         manager = emf.createEntityManager();
         
-        List<Calendario> calendario = (List<Calendario>) manager.createNamedQuery("FROM calendario").getResultList();
-        System.out.println("Hay" +calendario + " datos");
+        List<Calendario> calendario = (List<Calendario>) manager.createQuery("FROM Calendario").getResultList();
+        System.out.println("Hay " + calendario.size() + " datos");
     }
     
 }
